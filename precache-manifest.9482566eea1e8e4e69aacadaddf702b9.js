ff5c979647345d9c61f61e1e77935b9e80823232
Prathamesh More <pprathameshmore@outlook.com>
2020-06-09 10:05:45 +0530

    Updates

---
+++ precache-manifest.9482566eea1e8e4e69aacadaddf702b9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef88733f2738a763ee5321f4efda66a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "62d72ee3f9b2c92d5a64",
+    "url": "/portfolio/static/css/main.f25f7c3b.chunk.css"
+  },
+  {
+    "revision": "f3a3ef4f7118b1e8a342",
+    "url": "/portfolio/static/js/2.4b98f831.chunk.js"
+  },
+  {
+    "revision": "ccc502121842bd2ec2d999aa7d823359",
+    "url": "/portfolio/static/js/2.4b98f831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62d72ee3f9b2c92d5a64",
+    "url": "/portfolio/static/js/main.8dbba8dd.chunk.js"
+  },
+  {
+    "revision": "a73fea507bda4c56a0ff",
+    "url": "/portfolio/static/js/runtime-main.4b6a2a2d.js"
+  },
+  {
+    "revision": "87d994875c22d91270c9d27302a45fb7",
+    "url": "/portfolio/static/media/bootstrap.87d99487.png"
+  },
+  {
+    "revision": "df1d2359aae7f9411e1c768ccc998878",
+    "url": "/portfolio/static/media/mongodb.df1d2359.png"
+  },
+  {
+    "revision": "e15cf39a6053c2ae4e74d50f7d107cc8",
+    "url": "/portfolio/static/media/postgresql.e15cf39a.png"
+  },
+  {
+    "revision": "8d859602e4edf37f8e6fbce188479840",
+    "url": "/portfolio/static/media/react.8d859602.png"
+  },
+  {
+    "revision": "7a1ee97fdfd597c0b847f42969002a42",
+    "url": "/portfolio/static/media/tuition(1).7a1ee97f.jpg"
+  },
+  {
+    "revision": "914846307e6c0387f8b2d35163b88d8c",
+    "url": "/portfolio/static/media/tuition(2).91484630.jpg"
+  },
+  {
+    "revision": "d0f87a43090d69d7a572009c8542f534",
+    "url": "/portfolio/static/media/tuition(3).d0f87a43.jpg"
+  },
+  {
+    "revision": "97ada552f5c017c91b4b429089692a55",
+    "url": "/portfolio/static/media/tuition(4).97ada552.jpg"
+  },
+  {
+    "revision": "37e2247452c6d8341f5dc680ef7f8bea",
+    "url": "/portfolio/static/media/tuition(5).37e22474.jpg"
+  }
+]);
